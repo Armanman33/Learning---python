8430ea57b996f1f42459528dc311426907a13957
Arman <bayaterfan0@gmail.com>
2025-01-03 03:56:37 +0330

    Arranging, day 16

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip='Enter Todo')
 add_button = sg.Button('Add')
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]]) #each [] is a one row
+window = sg.Window('My To-Do App', layout=[[label], [input_box], [add_button]]) #each [] is a one row
 window.read()
 window.close()
